43456fccc451d6a3e44084c3e2681d1e8ae0d01a
Záruba Martin <martin.zaruba@artinsolution.com>
2020-11-12 11:53:27 +0100

    * unit tests for class and std syntax vue comp

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld
+      msg="Hello world component with standard syntax typescript component"
+    />
+    <HelloWorldClassSyntax
+      msg="Hello world component with class syntax typescript"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorldClassSyntax from "@/components/HelloWorldClassSyntax.vue";
+
+@Options({
+  components: {
+    HelloWorld,
+    HelloWorldClassSyntax
+  }
+})
+export default class Home extends Vue {}
+</script>
